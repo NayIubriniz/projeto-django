837990548de0a46d9d679b0c3dd759b5e548f075
Nayra Rodrigues <nayra_iubriniz@outlook.com>
2025-08-28 18:46:50 -0300

    'contato' e 'sobre' removidos

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,11 +8,3 @@ def home(request):
         'name': 'Nayra',
         'age': 30,
     })
-
-
-def contato(request):
-    return render(request, 'temp/temp.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
