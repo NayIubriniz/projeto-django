ade2cb156226ac262e5efb3cb541ad48cd87f26d
Nayra Rodrigues <nayra_iubriniz@outlook.com>
2025-10-20 22:51:10 -0300

    subindo atualizações no css

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,16 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html',)
+    return render(request, 'recipes/pages/home.html', {
+        'name': 'Nayra',
+        'age': 30,
+        'profission': 'Desenvolvedora',
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', {
+        'name': 'Nayra',
+        'age': 30,
+        'profission': 'Desenvolvedora',
+    })
